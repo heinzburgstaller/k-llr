6ae3aa50813ec0faa258d3aa7448b369c1189b3d
Heinz Burgstaller <heinz.burgstaller@student.tugraz.at>
2017-03-21 13:56:07 +0100

    Initial commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AlertModule } from 'ng2-bootstrap';
+import { ProgressbarModule } from 'ng2-bootstrap/progressbar';
+import { ModalModule } from 'ng2-bootstrap/modal';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ThemeComponent } from './theme/theme.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'theme', component: ThemeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ThemeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes),
+    AlertModule.forRoot(),
+    ProgressbarModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
