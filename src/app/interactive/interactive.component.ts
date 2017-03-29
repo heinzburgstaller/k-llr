1fc45d1420e6c551d43e2c153f831d1d4d096633
Heinz Burgstaller <heinz.burgstaller@student.tugraz.at>
2017-03-29 10:00:51 +0200

    added csv

--- src/app/interactive/interactive.component.ts
+++ src/app/interactive/interactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import { DataArray1 } from './data';
 
 @Component({
@@ -7,6 +8,8 @@ import { DataArray1 } from './data';
   styleUrls: ['./interactive.component.css']
 })
 export class InteractiveComponent implements OnInit {
+
+  private csvUrl: string = './original_data_500_rows.csv';
 
   public rows: Array<any> = [];
 
@@ -18,9 +21,49 @@ export class InteractiveComponent implements OnInit {
 
   @Output() onOk = new EventEmitter<any>();
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
+    this.readCsvData();
+  }
+
+  private readCsvData() {
+    debugger;
+    this.http.get(this.csvUrl)
+      .subscribe(
+      data => this.extractData(data),
+      err => this.handleError(err)
+      );
+  }
+
+  private extractData(res: Response) {
+    debugger;
+    let csvData = res['_body'] || '';
+    let allTextLines = csvData.split(/\r\n|\n/);
+    let headers = allTextLines[0].split(',');
+    let lines = [];
+
+    for (let i = 0; i < allTextLines.length; i++) {
+      // split content based on comma
+      let data = allTextLines[i].split(',');
+      if (data.length == headers.length) {
+        let tarr = [];
+        for (let j = 0; j < headers.length; j++) {
+          tarr.push(data[j]);
+        }
+        lines.push(tarr);
+      }
+    }
+    //this.csvData = lines;
+  }
+
+  private handleError(error: any) {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return errMsg;
   }
 
   setIndex(index: number): void {
