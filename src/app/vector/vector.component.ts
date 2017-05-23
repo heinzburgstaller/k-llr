2909117244d8851986dbc10c3f72dfeb1fd0f9bb
Heinz Burgstaller <heinz.burgstaller@student.tugraz.at>
2017-05-23 12:10:46 +0200

    Added use of custom vector for sangreea

--- src/app/vector/vector.component.ts
+++ src/app/vector/vector.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VectorHelper } from './vectorHelper';
 
 @Component({
   selector: 'app-vector',
@@ -7,12 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VectorComponent implements OnInit {
 
+  public radioModel: string = 'edu';
+
   public valueAge: number = 50;
   public valueEducation: number = 50;
   public valueWorkTime: number = 50;
   public valueCountry: number = 50;
   public valueSex: number = 50;
   public valueRelationship: number = 50;
+  public valueWorkclass: number = 50;
   public valueOccupation: number = 50;
   public valueIncome: number = 50;
   public valueRace: number = 50;
@@ -28,6 +32,7 @@ export class VectorComponent implements OnInit {
   public adjustedIncome: number = 50;
   public adjustedRace: number = 50;
   public adjustedMarital: number = 50;
+  public adjustedWorkclass: number = 50;
 
   constructor() { }
 
@@ -38,17 +43,57 @@ export class VectorComponent implements OnInit {
   valueChange(newValue) {
     var sum: number = this.valueAge + this.valueEducation + this.valueWorkTime
       + this.valueCountry + this.valueSex + this.valueRelationship + this.valueOccupation
-      + this.valueIncome + this.valueRace + this.valueMarital;
+      + this.valueIncome + this.valueRace + this.valueMarital + this.valueWorkclass;
+
+    if (this.radioModel == 'edu') {
+      sum = sum - this.valueEducation;
+    }
+
+    if (this.radioModel == 'marital') {
+      sum = sum - this.valueMarital;
+    }
+
+    if (this.radioModel == 'income') {
+      sum = sum - this.valueIncome;
+    }
+
     this.adjustedAge = this.valueAge / sum;
     this.adjustedEducation = this.valueEducation / sum;
     this.adjustedWorkTime = this.valueWorkTime / sum;
     this.adjustedCountry = this.valueCountry / sum;
     this.adjustedSex = this.valueSex / sum;
-    this.adjustedRelationship = this.valueRelationship/ sum;
+    this.adjustedRelationship = this.valueRelationship / sum;
     this.adjustedOccupation = this.valueOccupation / sum;
     this.adjustedIncome = this.valueIncome / sum;
     this.adjustedRace = this.valueRace / sum;
     this.adjustedMarital = this.valueMarital / sum;
+    this.adjustedWorkclass = this.valueWorkclass / sum;
+  }
+
+  public createVector(): any {
+    var v: any = {}
+    v['categorical'] = {};
+    if (this.radioModel != 'income') {
+      v['categorical']['income'] = this.adjustedIncome;
+    }
+    v['categorical']['native-country'] = this.adjustedCountry;
+    v['categorical']['sex'] = this.adjustedSex;
+    v['categorical']['relationship'] = this.adjustedRelationship;
+    v['categorical']['workclass'] = this.adjustedWorkclass;
+    v['categorical']['race'] = this.adjustedRace;
+    v['categorical']['occupation'] = this.adjustedOccupation;
+    if (this.radioModel != 'marital') {
+      v['categorical']['marital-status'] = this.adjustedMarital;
+    }
+
+    v['range'] = {};
+    v['range']['age'] = this.adjustedAge;
+    v['range']['hours-per-week'] = this.adjustedWorkTime;
+    if (this.radioModel != 'edu') {
+      v['range']['education-num'] = this.adjustedEducation;
+    }
+
+    return v;
   }
 
 }
