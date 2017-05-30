2523febb97242c869b3fb57d55f5b1bbcb44937d
Heinz Burgstaller <heinz.burgstaller@student.tugraz.at>
2017-05-30 09:07:13 +0200

    Proved working API

--- src/app/vector/vectorHelper.spec.ts
+++ src/app/vector/vectorHelper.spec.ts
@@ -1,7 +1,7 @@
 import { VectorComponent } from './vector.component';
 import { VectorHelper } from './vectorHelper';
-import { SaNGreeA, ISaNGreeAConfig } from 'anonymiationjs';
-import * as $A from 'anonymiationjs';
+import { SaNGreeA, ISaNGreeAConfig } from 'anonymizationjs';
+import * as $A from 'anonymizationjs';
 
 describe('Vector Helper', () => {
   it('should recalculate the vector', () => {
@@ -17,25 +17,41 @@ describe('Vector Helper', () => {
     config.K_FACTOR = 2;
     config['GEN_WEIGHT_VECTORS']['equal'] = v;
 
-    var sangreea = new $A.algorithms.Sangreea("testus", config);
+    var sangreea:SaNGreeA = new $A.algorithms.Sangreea("testus", config);
     var reducers: Map<string, number> = new Map();
     reducers.set('age', 0.12);
+    reducers.set('workclass', 0.08);
     VectorHelper.reduce(sangreea, reducers, 0.1);
 
-    var value2: number = config['GEN_WEIGHT_VECTORS']['equal']['range']['age'];
-    var value3: number = config['GEN_WEIGHT_VECTORS']['equal']['range']['hours-per-week'];
+    var value2: number = sangreea.getConfig()['GEN_WEIGHT_VECTORS']['equal']['range']['age'];
     expect(value2).toBeCloseTo(0.09784, 0.000001);
-    expect(value3).toBeCloseTo(0.10024, 0.000001);
 
     var sum: number = 0.0;
-    Object.keys(config['GEN_WEIGHT_VECTORS']['equal']['categorical']).forEach((feature) => {
-      sum += config['GEN_WEIGHT_VECTORS']['equal']['categorical'][feature];
+    Object.keys(sangreea.getConfig()['GEN_WEIGHT_VECTORS']['equal']['categorical']).forEach((feature) => {
+      sum += sangreea.getConfig()['GEN_WEIGHT_VECTORS']['equal']['categorical'][feature];
     });
-    Object.keys(config['GEN_WEIGHT_VECTORS']['equal']['range']).forEach((feature) => {
-      sum += config['GEN_WEIGHT_VECTORS']['equal']['range'][feature];
+    Object.keys(sangreea.getConfig()['GEN_WEIGHT_VECTORS']['equal']['range']).forEach((feature) => {
+      sum += sangreea.getConfig()['GEN_WEIGHT_VECTORS']['equal']['range'][feature];
     });
 
     expect(sum).toBeCloseTo(1.0000, 0.000001);
+
+    var reducers: Map<string, number> = new Map();
+    reducers.set('relationship', 0.22);
+    reducers.set('age', 0.11);
+    reducers.set('occupation', 0.33);
+    VectorHelper.reduce(sangreea, reducers, 0.9);
+
+    sum = 0.0;
+    Object.keys(sangreea.getConfig()['GEN_WEIGHT_VECTORS']['equal']['categorical']).forEach((feature) => {
+      sum += sangreea.getConfig()['GEN_WEIGHT_VECTORS']['equal']['categorical'][feature];
+    });
+    Object.keys(sangreea.getConfig()['GEN_WEIGHT_VECTORS']['equal']['range']).forEach((feature) => {
+      sum += sangreea.getConfig()['GEN_WEIGHT_VECTORS']['equal']['range'][feature];
+    });
+
+    expect(sum).toBeCloseTo(1.0000, 0.000001);
+
   });
 
 });
